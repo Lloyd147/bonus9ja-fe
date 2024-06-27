77cd1a7786b86f4126b16239f8808ccfe4b10f35
Muneeb Shah <iamabdulmuneeb@gmail.com>
2024-06-27 19:00:32 +0500

    Create admin footer form compoenent

--- src/app/admin/footer/page.tsx
+++ src/app/admin/footer/page.tsx
@@ -1,30 +1,8 @@
-import ToggleSwitch from "@/components/ToggleSwitch";
-import React from "react";
+import AdminFooter from '@/components/AdminFooter';
+import React from 'react';
 
 const AdminFooterPage = async () => {
-  return (
-    <div className="wrapper">
-      <div className="button-row">
-        <h1>Footer</h1>
-      </div>
-      <div className="footer-options">
-        <div className="options__header row">
-          <div className="header__option footer-cell">On/Off</div>
-          <div className="header__option footer-cell">Page</div>
-          <div className="header__option footer-cell">Actions</div>
-        </div>
-        <div className="options__body row">
-          <div className="body__option footer-cell">
-            <ToggleSwitch />
-          </div>
-          <div className="body__option footer-cell">All footers</div>
-          <div className="body__option footer-cell">
-            <img className="edit-icon" src="/images/edit.svg" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <AdminFooter />;
 };
 
 export default AdminFooterPage;
---
+++ src/components/FooterForm/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FooterForm = () => {
+  return <div className="footer-form">FooterForm</div>;
+};
+
+export default FooterForm;
